cbb23d588585c8aff47f11559abf56c82904dd2e
sallystcy <68220736+sallystcy@users.noreply.github.com>
2020-07-16 08:40:13 +0100

    Add stock marekt comparison plots to jupyter
    Adding missed function to jupyter and other edits (Also copied jupyter code into .py file). Compte with stock market anaylysis - saved in the .csv file.

--- newsAPI_sentiment.py
+++ newsAPI_sentiment.py
@@ -7,6 +7,17 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
+# Use preloaded market data file to compare to news about covid
+market_data_file = '~/library/SP500_Fund_Prices.csv'
+
+def authenticate_client(key, endpoint):
+    # Authenticate the client
+    
+    credential = AzureKeyCredential(key)
+    text_analytics_client = TextAnalyticsClient(endpoint=endpoint, credential=credential)
+    return text_analytics_client
+
+
 def getNews(qPhrase):
     # Retrieving news results and formatting into pandas df
     
@@ -67,17 +78,6 @@ def getNews(qPhrase):
     return df
 
 
-def split_date_time_to_obj(date_time_string):
-    # Split the date_time string since it is in the format yyyy-mm-ddTHH:MM:SS
-    # Returns a datetime object
-
-    datetime_str = ''.join(' '.join(date_time_string.split('T')).split('Z'))
-    datetime_obj = datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S')
-
-    return datetime_obj
-    return df
-    
-
 def getSentiment(doc):
     # Sentiment analysis
     
@@ -96,21 +96,30 @@ def getSentiment(doc):
 
     return response.sentiment, response.confidence_scores.positive, response.confidence_scores.neutral, response.confidence_scores.negative  
 
-def newsToCsv(newsdf, subject, country = None):
-    # Save data to a csv file.
-    # NOTE: FILE PATH WILL NEED TO BE CHANGED DEPENDING ON JUPYTER OR VS/PYCHARM USE
 
+def split_date_time_to_obj(date_time_string):
+    # Split the date_time string since it is in the format yyyy-mm-ddTHH:MM:SS
+    # Returns a datetime object
+
+    datetime_str = ''.join(' '.join(date_time_string.split('T')).split('Z'))
+    datetime_obj = datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S')
+
+    return datetime_obj
+    return df
+    
+    
+def newsToCsv(newsdf, subject, country = None):
+    # Save dataframe to a csv file - currently saving to jupyter notebook library
+    
     if country is None:
         country = 'gen' # general
 
-    file_path_root = 'C:/temp/'
+    file_path_root = '~/library/'
     file_name = "iBankSentimentAnalysis_" + country + "_" + subject + ".csv"
     
     # Save dataframe to csv file in jupyter project folder
     newsdf.to_csv(file_path_root + file_name, index = False)  
 
-
-# NOTE: NOT SURE IF PLOTTING WILL WORK IN VS/PYCHARM SO FOR USE IN JUPYTER
 def plotRes(df, subject = ''):
     # Plot overall sentiment against date
     
@@ -124,17 +133,20 @@ def plotRes(df, subject = ''):
     ax.set_title('Overall Sentiment of ' + subject + ' news')
     
     # Format x axis dates
+    format_dates(ax)
+    
+    plt.show()
+    
+def format_dates(ax):
+    # Format x axis dates
     weeks = mdates.WeekdayLocator(byweekday=1, interval=1, tz=None)
     weeks_fmt = mdates.DateFormatter('%Y-%m-%d')
     days = mdates.DayLocator(bymonthday=None, interval=1, tz=None)
     ax.xaxis.set_major_locator(weeks)
     ax.xaxis.set_major_formatter(weeks_fmt)
     ax.xaxis.set_minor_locator(days)
-    
-    plt.show()
-    
-    
-def plotCompare(df1, df2):
+
+def plotCompareNews(df1, df2):
     # Compare the overall sentiment of two dfs filled with news results 
     # e.g. df1 hsa covid results and df2 has market results 
     
@@ -162,14 +174,45 @@ def plotCompare(df1, df2):
     plt.legend(bbox_to_anchor=(1.05, 1))
     
     # Format x axis dates
-    weeks = mdates.WeekdayLocator(byweekday=1, interval=1, tz=None)
-    weeks_fmt = mdates.DateFormatter('%Y-%m-%d')
-    days = mdates.DayLocator(bymonthday=None, interval=1, tz=None)
-    ax.xaxis.set_major_locator(weeks)
-    ax.xaxis.set_major_formatter(weeks_fmt)
-    ax.xaxis.set_minor_locator(days)
+    format_dates(ax)
     
     plt.show()
 
-    if __name__ == '__main__':
-        getNews_new('coronavirus')
+def compareMarketandCovid(df_c, market_data_file):
+    # Read Market data csv into a pandas df and format
+    md_df = pd.read_csv(market_data_file)
+    headers = md_df.columns
+    date_col = headers[0]
+    
+    # Change date col to datetime objs and set as index
+    md_df[date_col] = pd.to_datetime(md_df[date_col])
+    md_df = md_df.set_index(keys = date_col, drop = True)
+    
+    # Only compare for dates where we have sentiment analysis of news
+    start_date = df_c['publishedAt'][0]
+    end_date = df_c['publishedAt'][len(df_c['publishedAt']) - 1]
+    
+    # Plot 
+    fig, ax = plt.subplots()
+    lines = plt.plot(md_df)
+    for i in range(0, len(md_df.columns)):
+        lines[i].set_label(md_df.columns[i])
+    plt.legend(bbox_to_anchor=(1.15, 1))
+    
+    #plt.legend(md_df.columns, bbox_to_anchor=(1.05, 1))
+    ax2=ax.twinx()
+    plt.scatter(df_c['publishedAt'].values, df_c['Overall Sentiment'].values, c = 'purple', marker = 'x', label = 'Covid news sentiments')
+    
+    #Display a legend to help navigate the graph
+    ax.set_xlim([start_date, end_date])
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Stock Price')
+    ax2.set_ylabel('Overall Sentiment')
+    ax.set_title('Comparing Stock Price and Overall Sentiment of Covid news')
+    
+    # Format x axis dates
+    format_dates(ax)
+    
+    plt.legend(bbox_to_anchor=(1.63, 0.7))
+    
+    plt.show()
